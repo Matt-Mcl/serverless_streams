c87d7221fb84f19d36ae5221d32d248777571a25
Matt-Mcl <mattmcl2000@gmail.com>
2022-03-31 16:09:56 +0000

    Updates logic to support send/rec streams

--- stand_alone/basic_kafka_example/main.py
+++ stand_alone/basic_kafka_example/main.py
@@ -1,10 +1,10 @@
 from kafka import KafkaConsumer, KafkaProducer
 import certifi
-from datetime import datetime
 import time
 
 __REGION='uk-london-1'
-__STREAM_NAME='send4'
+__ORIGIN_STREAM='send'
+__DESTINATION_STREAM='rec'
 __TENANCY_NAME='ecrcloud'
 __STREAMPOOL_OCID='ocid1.streampool.oc1.uk-london-1.amaaaaaatwfhi7yalnuxxqgad2opy5kainapshftx5kurpjfjzcyacs5e4nq'
 __OCI_USERNAME='oracleidentitycloudservice/matthew.mcloughlin@oracle.com'
@@ -14,42 +14,55 @@ __BOOTSTRAP_SERVER='cell-1.streaming.'+__REGION+'.oci.oraclecloud.com'
 
 # print(__SASL_USERNAME)
 
-before_produce = time.time()
-producer = KafkaProducer(
-    bootstrap_servers=__BOOTSTRAP_SERVER,
-    security_protocol='SASL_SSL',
-    sasl_mechanism='PLAIN',
-    sasl_plain_username=__SASL_USERNAME,
-    sasl_plain_password=__SASL_TOKEN,
-    ssl_certfile=certifi.where(),
-)
+
+def oci_consumer():
+    before_consume = time.time()
+    consumer = KafkaConsumer(
+        bootstrap_servers=__BOOTSTRAP_SERVER,
+        security_protocol='SASL_SSL',
+        sasl_mechanism='PLAIN',
+        sasl_plain_username=__SASL_USERNAME,
+        sasl_plain_password=__SASL_TOKEN,
+        ssl_certfile=certifi.where(),
+        group_id="my_group",
+    )
+
+    consumer.subscribe([__ORIGIN_STREAM])
+
+    data = consumer.poll(1000)
+
+    consumer.commit()
+    consumer.close()
+    after_consume = time.time()
+    print("Consume time: " + str(after_consume - before_consume))
+
+    return data
 
 
-future = producer.send(__STREAM_NAME, b'some_message_bytes: '+str(datetime.now()).encode('ascii'))
-after_produce = time.time()
+def oci_producer(data):
+    before_produce = time.time()
+    producer = KafkaProducer(
+        bootstrap_servers=__BOOTSTRAP_SERVER,
+        security_protocol='SASL_SSL',
+        sasl_mechanism='PLAIN',
+        sasl_plain_username=__SASL_USERNAME,
+        sasl_plain_password=__SASL_TOKEN,
+        ssl_certfile=certifi.where(),
+    )
 
-before_consume = time.time()
-consumer = KafkaConsumer(
-    bootstrap_servers=__BOOTSTRAP_SERVER,
-    security_protocol='SASL_SSL',
-    sasl_mechanism='PLAIN',
-    sasl_plain_username=__SASL_USERNAME,
-    sasl_plain_password=__SASL_TOKEN,
-    ssl_certfile=certifi.where(),
-    group_id="my_group",
-)
+    for k, v in data.items():
+        for item in v:
+            to_send = b'Recieved: '+str(item.value).encode('ascii')
+            print(to_send)
+            producer.send(__DESTINATION_STREAM, to_send)
 
-consumer.subscribe([__STREAM_NAME])
+    after_produce = time.time()
 
-data = consumer.poll(1000)
+    print("Produce time: " + str(after_produce - before_produce))
 
-for k, v in data.items():
-    print(v[0].value)
-
-consumer.commit()
-consumer.close()
-after_consume = time.time()
+    return
 
 
-print("Produce time: " + str(after_produce - before_produce))
-print("Consume time: " + str(after_consume - before_consume))
+if __name__ == "__main__":
+    data = oci_consumer()
+    oci_producer(data)
